1b1809d64a6b8d568fa0d0b11b3e078a504d0c7f
Brendan Rehman <brendan@ignitedcoder.com>
2015-08-23 16:08:08 -0700

    Dropdown helper sample controller.

--- application/controllers/Dropdown.php
+++ application/controllers/Dropdown.php
@@ -38,11 +38,11 @@ SQL;
         $dropDownData = $this->getListOfItemsFromTable();
 
         $this->viewData['my_ddl'] = build_dropdown('clinic_dropdown', //the id of your dropdown list
-            'form-control', //the style class for your dropdown list
-            'clinic_dropdown', //the name of your dropdown list
-            $dropDownData, // they key/value pair for your dropdown list.
-            $this->uri->segment(2), //e.g. if you are using the URI to select a value from the dropdown www.mydomain.com/item/value
-            'javaScriptFunctionName(this);');  //A javascript function to call when the selection changes.
+                                    'form-control', //the style class for your dropdown list
+                                    'clinic_dropdown', //the name of your dropdown list
+                                    $dropDownData, // they key/value pair for your dropdown list.
+                                    $this->uri->segment(2), //e.g. if you are using the URI to select a value from the dropdown www.mydomain.com/item/value
+                                    'javaScriptFunctionName(this);');  //A javascript function to call when the selection changes.
         $this->load->view('myview',$this->viewData);
         //inside your view simply echo $my_ddl to produce the dropdown list.
         //thats it.
